6a3ab11d1175509ce800c116a051278a03f74ba4
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-02-18 01:19:21 +0500

    tes

--- application/resources/views/presets/default/sections/banner.blade.php
+++ application/resources/views/presets/default/sections/banner.blade.php
@@ -768,7 +768,7 @@
 
     <div class="container">
         <div class="row gy-4 mb-5 marg">
-            <div class="col-lg-8 col-md-12 d-flex ">
+            <div class="col-lg-12 col-md-12 d-flex ">
                 <div class="hero-left-content">
                     <h2 class="hero-title wow animate__animated animate__fadeInUp" data-wow-delay="0.2s">
                         {{__(@$bannerSection->data_values?->heading) }}</h2>
